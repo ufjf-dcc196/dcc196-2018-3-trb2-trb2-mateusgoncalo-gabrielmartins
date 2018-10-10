69cdde14c10d7b64f2abf64ee0878c44a4eae647
Mateus Gonçalo <mateusgon57@gmail.com>
2018-10-10 20:14:18 -0300

    Criando o SerieDbHelper

--- app/src/main/java/com/example/mateu/dcc196_exercicio02/SerieContract.java
+++ app/src/main/java/com/example/mateu/dcc196_exercicio02/SerieContract.java
@@ -8,12 +8,12 @@ public class SerieContract {
         public final static String COLUMN_NAME_NOME = "nome";
         public static final String COLUMN_NAME_TEMPORADA = "temporada";
         public static final String COLUMN_NAME_EPISODIO = "episodio";
-        public final static String CREATE_LIVRO  = "CREATE TABLE "+Serie.TABLE_NAME+" ("
+        public final static String CREATE_SERIE  = "CREATE TABLE "+Serie.TABLE_NAME+" ("
                 + Serie._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + Serie.COLUMN_NAME_NOME+ " TEXT, "
                 + Serie.COLUMN_NAME_TEMPORADA+ " INTEGER,"
                 + Serie.COLUMN_NAME_EPISODIO+ " INTEGER"
                 +")";
-        public final static String DROP_LIVRO = "DROP TABLE IF EXISTS "+Serie.TABLE_NAME;
+        public final static String DROP_SERIE = "DROP TABLE IF EXISTS "+Serie.TABLE_NAME;
     }
 }
---
+++ app/src/main/java/com/example/mateu/dcc196_exercicio02/SerieDbHelper.java
@@ -0,0 +1,32 @@
+package com.example.mateu.dcc196_exercicio02;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SerieDbHelper extends SQLiteOpenHelper{
+    
+    public final static int DATABASE_VERSION = 1;
+    public final static String DATABASE_NAME = "Serie.db";
+
+    public SerieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SerieContract.Serie.CREATE_SERIE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SerieContract.Serie.DROP_SERIE);
+        onCreate(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+}
